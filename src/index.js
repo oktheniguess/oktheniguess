8d2a87a7704b763896d1bb6c9ec4dc7e81b0a4d8
oktheniguess <63266962+oktheniguess@users.noreply.github.com>
2021-06-30 18:32:31 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Viewer!</h1>
+<div>
+I finished my first ever website! I made it easy, in order to view it, just press right  here->
+  <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noopener noreferrer">www.sander.com</a>.
+</div>
+`;
